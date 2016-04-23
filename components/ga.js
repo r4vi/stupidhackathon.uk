3af23261c580042893665d6685d2acdf8ff377f1
Ravi Kotecha <kotecha.ravi@gmail.com>
2016-04-23 11:10:12 +0100

    stop elint crying

--- components/ga.js
+++ components/ga.js
@@ -1,27 +1,31 @@
+/* global ga */
 import React, { Component } from 'react'
+import { config } from 'config'
 
 class GA extends Component {
   componentDidMount () {
-    (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+    /* eslint-disable */
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,"script","https://www.google-analytics.com/analytics.js","ga")
-    ga("create", "UA-46090922-5", "auto")
-    ga("send", "pageview")
+    })(window,document,'script','https://www.google-analytics.com/analytics.js', 'ga')
+    /* eslint-enable */
+    ga('create', config.gaId, 'auto')
+    ga('send', 'pageview')
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-    return this.props.path != nextProps.path
+  shouldComponentUpdate (nextProps) {
+    return this.props.path !== nextProps.path
   }
-  componentDidUpdate (prevProps, prevState) {
-    ga("send", "pageview")
+  componentDidUpdate () {
+    ga('send', 'pageview', this.props.path)
   }
-    
   render () {
     return (<script></script>)
   }
-
-  should
+}
+GA.propTypes = {
+  path: React.PropTypes.string,
 }
 
 export default GA
